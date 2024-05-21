3462698bd6dc7c568a6180ac0beefa9e4bb46c81
Dize dev <axiozweb2022@gmail.com>
2024-05-21 12:35:29 -0500

    funcionalidad del menu al mostrar y ocultal

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@ const formPopup = document.querySelector('.form-popup');
 const showPopupBtn = document.querySelector('.login-btn');
 const closePopupBtn = document.querySelector('.form-popup .close-btn');
 const loginSiginLink = document.querySelectorAll('.form-box .bottom-links a');
+const menuBtn = document.querySelector('.navbar .menu-btn');
+const navBarMEnu = document.querySelector('.navbar .links');
+const closeMenuBtn = document.querySelector('.links .close-btn');
 
 showPopupBtn.addEventListener('click', () => {
     document.body.classList.toggle('show-popup');
 });
+
+closeMenuBtn.addEventListener('click', () => menuBtn.click());
 
 closePopupBtn.addEventListener('click', () => showPopupBtn.click());
 
@@ -21,3 +26,7 @@ closePopupBtn.addEventListener('click', () => showPopupBtn.click());
             'remove']('show-signup');
     });
 });
+
+menuBtn.addEventListener('click', () => {
+    navBarMEnu.classList.toggle('show-menu')
+})
