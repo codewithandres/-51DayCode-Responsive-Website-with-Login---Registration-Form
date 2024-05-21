cfaa1d7703bf036fd37ebc7b0a5760cbbbd7511a
Dize dev <axiozweb2022@gmail.com>
2024-05-21 11:42:00 -0500

    funcionalidad para cambiar de formulario de secion ha formualrio de reguistro

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 
+const formPopup = document.querySelector('.form-popup');
 const showPopupBtn = document.querySelector('.login-btn');
 const closePopupBtn = document.querySelector('.form-popup .close-btn');
-
+const loginSiginLink = document.querySelectorAll('.form-box .bottom-links a');
 
 showPopupBtn.addEventListener('click', () => {
     document.body.classList.toggle('show-popup');
 });
 
 closePopupBtn.addEventListener('click', () => showPopupBtn.click());
+
+[...loginSiginLink].map(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        formPopup.classList[link.id === 'login-link'
+            ?
+            'add'
+            :
+            'remove']('show-signup');
+    });
+});
